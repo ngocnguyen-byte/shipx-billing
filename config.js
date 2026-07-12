ddab236912c27911b0aa23cf1dd22f5e2d2b14d6
ngocnguyen-byte <ngoc.nguyen@amilo.co>
2026-07-12 12:23:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SHIPX_CONFIG = {
   SUPABASE_URL:      "https://zvrxsbqggovvxtbisibm.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_sb_publishable_72z_KVUE8suV2lbIFXzCVg_t37x4md8"
+  SUPABASE_ANON_KEY: "sb_publishable_72z_KVUE8suV2lbIFXzCVg_t37x4md8"
 };
